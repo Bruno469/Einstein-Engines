408b0d2ffcd52439d26d0e2aa78ce924fe7ea5bc
bruno469 <bruno319124@gmail.com>
2024-05-17 23:08:16 -0300

    Basic Action

---
+++ Content.Shared/Species/Components/MothFlyComponent.cs
@@ -0,0 +1,31 @@
+using Content.Shared.Mobs;
+using Robust.Shared.Prototypes;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.Species.Components;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class MothFlyComponent : Component
+{
+    /// <summary>
+    /// The action to use.
+    /// </summary>
+    [DataField("actionPrototype", required: true)]
+    public EntProtoId ActionPrototype;
+
+    [DataField, AutoNetworkedField]
+    public EntityUid? ActionEntity;
+
+    /// <summary>
+    /// What mob states the action will appear in
+    /// </summary>
+    [DataField("allowedStates"), ViewVariables(VVAccess.ReadWrite)]
+    public List<MobState> AllowedStates = new();
+
+    /// <summary>
+    /// The text that appears when attempting to split.
+    /// </summary>
+    [DataField("popupText")]
+    public string PopupText = "moth-fly-action-use";
+}
+
