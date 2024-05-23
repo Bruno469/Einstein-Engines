2a981f91144d44d13befba187081d4295d622b68
bruno469 <bruno319124@gmail.com>
2024-05-23 18:55:14 -0300

    Add MothFly client-server

--- Content.Shared/Species/Systems/SharedMothFlySystem.cs
+++ Content.Shared/Species/Systems/SharedMothFlySystem.cs
@@ -5,22 +5,29 @@ using Content.Shared.Mobs;
 using Content.Shared.Mobs.Components;
 using Content.Shared.Popups;
 using Robust.Shared.Prototypes;
-
+using Content.Shared.Damage.Systems;
+using Content.Shared.Movement.Events;
+using Robust.Shared.Physics.Components;
+using Robust.Shared.Physics.Systems;
+using Content.Shared.Movement.Systems;
 
 namespace Content.Shared.Species;
 
-public sealed partial class MothFlySystem : EntitySystem
+public sealed partial class SharedMothFlySystem : EntitySystem
 {
     [Dependency] private readonly SharedActionsSystem _actionsSystem = default!;
     [Dependency] private readonly SharedBodySystem _bodySystem = default!;
     [Dependency] private readonly IPrototypeManager _protoManager = default!;
     [Dependency] private readonly SharedPopupSystem _popupSystem = default!;
+    [Dependency] private readonly SharedPhysicsSystem _physics = default!;
+    [Dependency] private readonly MovementSpeedModifierSystem _movementSpeedModifier = default!;
+    [Dependency] private readonly StaminaSystem _stamina = default!;
 
     public override void Initialize()
     {
         base.Initialize();
         SubscribeLocalEvent<MothFlyComponent, MobStateChangedEvent>(OnMobStateChanged);
-        SubscribeLocalEvent<MothFlyComponent, BaseActionEvent>(OnFlyAction);
+        SubscribeLocalEvent<MothFlyComponent, ToggleMothflyEvent>(OnFlyAction);
     }
 
     private void OnMobStateChanged(EntityUid uid, MothFlyComponent comp, MobStateChangedEvent args)
@@ -46,8 +53,40 @@ public sealed partial class MothFlySystem : EntitySystem
 
     private void OnFlyAction(EntityUid uid, MothFlyComponent comp, BaseActionEvent args)
     {
-        // When they use the action, gib them.
+        // When they use the action, gib them (It's a meme lol).
         _popupSystem.PopupClient(Loc.GetString(comp.PopupText, ("name", uid)), uid, uid);
-        _bodySystem.GibBody(uid, true);
+        if (TryComp<PhysicsComponent>(uid, out var physics))
+            _stamina.TakeStaminaDamage(uid, 50);
+    }
+
+    private bool IsEnabled(EntityUid uid)
+    {
+        return HasComp<MothFlyComponent>(uid);
+    }
+
+    protected virtual bool CanEnable(EntityUid uid, MothFlyComponent component)
+    {
+        return true;
+    }
+    public void SetEnabled(EntityUid uid, MothFlyComponent component, bool enabled)
+    {
+        var user = component.Owner;
+
+        if (IsEnabled(uid) == enabled ||
+            enabled && !CanEnable(uid, component))
+        {
+            if (TryComp<PhysicsComponent>(user, out var physics))
+                _physics.SetBodyStatus(user, physics, BodyStatus.OnGround);
+            return;
+        }
+
+        if (TryComp<PhysicsComponent>(user, out var newphysics))
+            _physics.SetBodyStatus(user, newphysics, BodyStatus.InAir);
+        _movementSpeedModifier.ChangeBaseSpeed(user, 2, 2, 1);
+    }
+
+    public bool IsUserFlying(EntityUid uid)
+    {
+        return HasComp<MothFlyComponent>(uid);
     }
 }
