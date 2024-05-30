c28163ee556865f36b47cf811a6b46db52777fc0
Bruno <bruno319124@gmail.com>
2024-05-30 18:04:00 -0300

    Now it's possible use

--- Content.Shared/Species/Components/MothFlyComponent.cs
+++ Content.Shared/Species/Components/MothFlyComponent.cs
@@ -27,5 +27,7 @@ public sealed partial class MothFlyComponent : Component
     /// </summary>
     [DataField("popupText")]
     public string PopupText = "moth-fly-action-use";
+    [DataField("popupText")]
+    public string StopPopupText = "moth-fly-stop-action-use";
 }
 
